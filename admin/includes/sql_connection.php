bbb1f9d1775c278187b4fc05d0d62d6fc1aac0a7
Lim Jian Hui <63503415+limjianhui789@users.noreply.github.com>
2023-05-03 23:36:26 +0800

    Update

--- admin/includes/sql_connection.php
+++ admin/includes/sql_connection.php
@@ -1,6 +1,6 @@
 <?php
-	DEFINE ('DB_HOST', 'ce-database.cbe1mmzjy0xh.us-east-1.rds.amazonaws.com:3306');
+	DEFINE ('DB_HOST', 'ce-database.crh2wdilhyph.us-east-1.rds.amazonaws.com:3306');
 	DEFINE ('DB_USER', 'admin');
-	DEFINE ('DB_PASSWORD', 'ceadmin-db');
+	DEFINE ('DB_PASSWORD', 'ce_admin');
 	DEFINE ('DB_NAME', 'CreativeEvent');
 ?>
--- includes/database-connector.php
+++ includes/database-connector.php
@@ -3,10 +3,10 @@
         DEFINE ('DB_USER', 'admin');
     }
     if(defined('DB_PASSWORD') == false){
-        DEFINE ('DB_PASSWORD', 'ceadmin-db');
+        DEFINE ('DB_PASSWORD', 'ce_admin');
     }
     if(defined('DB_HOST') == false){
-        DEFINE ('DB_HOST', 'ce-database.cbe1mmzjy0xh.us-east-1.rds.amazonaws.com:3306');
+        DEFINE ('DB_HOST', 'ce-database.crh2wdilhyph.us-east-1.rds.amazonaws.com:3306');
     }
     if(defined('DB_NAME') == false){
         DEFINE ('DB_NAME', 'CreativeEvent');
