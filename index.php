e06365ccfd6192281ab6f03a0624ad4c9a34ce2a
Lim Jian Hui <63503415+limjianhui789@users.noreply.github.com>
2023-05-06 03:02:38 +0800

    Update the code

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NitroGaming</title>
+    <title>Creative Event</title>
     <link rel="icon" type="image/x-icon" href="image/favicon.ico">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdel^vr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"/>
